4d40ce46c1b61945386d00d4cd9d6d704dfc4941
Montacer Dkhilali <montacer.dkhilali@unicorne.cloud>
2023-10-09 22:23:22 -0400

    Add Transaltion files

---
+++ react-ts/src/translation/en.ts
@@ -0,0 +1,14 @@
+const englishTranslations = {
+  Hello: "Hello",
+  World: "World",
+  Welcome: "Welcome",
+  Params: {
+    VeryHigh: "Very High (5)",
+    High: "High (4)",
+    Medium: "Medium (3)",
+    Low: "Low (2)",
+    VeryLow: "Very Low (1)",
+  },
+};
+
+export default englishTranslations;
---
+++ react-ts/src/translation/fr.ts
@@ -0,0 +1,14 @@
+const FrenchTranslations = {
+  Hello: "Bonjour",
+  World: "Le monde",
+  Welcome: "Bienvenue",
+  Params: {
+    VeryHigh: "Très élevé (5)",
+    High: "Élevé (4)",
+    Medium: "Moyen (3)",
+    Low: "Faible (2)",
+    VeryLow: "Très faible (1)",
+  },
+};
+
+export default FrenchTranslations;
